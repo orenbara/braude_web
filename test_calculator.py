3ff516c35bc515d2d52a7f4c89a038400253226e
OrenBaranovsky <98653232+orenbara@users.noreply.github.com>
2025-01-24 19:31:33 +0200

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+# test_calculator.py
+
+from calculator import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(0, 5) == -5
